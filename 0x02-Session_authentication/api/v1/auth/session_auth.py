eabbc245dc1903b8bbb35b472622e365b372b3d4
horpeazy <iyamuhope.nosa647@gmail.com>
2023-08-10 03:47:50 +0100

    feat: session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,3 +15,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ gets a user id based on session id """
+        if session_id is None or type(session_id) is not str:
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
