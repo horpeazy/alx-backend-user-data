4be7b0d9ca787f44b20c311332e1219ce8fdb98e
horpeazy <iyamuhope.nosa647@gmail.com>
2023-08-15 02:06:01 +0100

    feat: user auth service

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,65 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+        """ adds a user to the database """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> TypeVar("User"):
+        """ finds a user by the arbitrary inouts """
+        try:
+            query = self._session.query(User).filter_by(**kwargs)
+            result = query.first()
+            if result is None:
+                raise NoResultFound
+            return result
+        except InvalidRequestError as e:
+            self._session.rollback()
+            raise e
+
+    def update_user(self, user_id, **kwargs):
+        """ updates the user """
+        if not user_id:
+            return None
+        user = self.find_user_by(id=user_id)
+        if not user:
+            return None
+        for key, value in kwargs.items():
+            attr = getattr(User, key, None)
+            if attr is None:
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
